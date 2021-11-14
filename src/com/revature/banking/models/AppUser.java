7d98f268010bc9bce2422733346bb7f33f9d5cc7
Tommy Nguyen <minhtuan@vt.edu>
2021-11-14 15:28:03 -0500

    ignoring properties file

--- src/com/revature/banking/models/AppUser.java
+++ src/com/revature/banking/models/AppUser.java
@@ -5,15 +5,16 @@ import java.util.Objects;
 /*
     POJO : simple encapsulation of data. they do not have rich features, they simply hold related values.
  */
-public class AppUser {
+public class Customer {
 
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
     private String username;
     private String password;
 
-    public AppUser(String firstName, String lastName, String email, String username, String password) {
+    public Customer(String firstName, String lastName, String email, String username, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -21,29 +22,58 @@ public class AppUser {
         this.password = password;
     }
 
-    public String getFirstName() {return firstName;}
+    public String getId() {
+        return id;
+    }
 
-    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public String getLastName() {return lastName;}
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-    public String getEmail() {return email;}
+    public String getLastName() {
+        return lastName;
+    }
 
-    public void setEmail(String email) {this.email = email;}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-    public String getUsername() {return username;}
+    public String getEmail() {
+        return email;
+    }
 
-    public void setUsername(String username) {this.username = username;}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getPassword() {return password;}
+    public String getUsername() {
+        return username;
+    }
 
-    public void setPassword(String password) {this.password = password;}
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String toFileString() {
         StringBuilder builder = new StringBuilder();
-        builder.append(firstName).append(":")
+        builder.append(id).append(":")
+                .append(firstName).append(":")
                 .append(lastName).append(":")
                 .append(email).append(":")
                 .append(username).append(":")
@@ -52,30 +82,28 @@ public class AppUser {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer appUser = (Customer) o;
+        return Objects.equals(id, appUser.id) && Objects.equals(firstName, appUser.firstName) && Objects.equals(lastName, appUser.lastName) && Objects.equals(email, appUser.email) && Objects.equals(username, appUser.username) && Objects.equals(password, appUser.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, username, password);
+    }
+
+    @Override
     public String toString() {
         return "AppUser{" +
-                "firstName='" + firstName + '\'' +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AppUser appUser = (AppUser) o;
-        return Objects.equals(firstName, appUser.firstName)
-                && Objects.equals(lastName, appUser.lastName)
-                && Objects.equals(email, appUser.email)
-                && Objects.equals(username, appUser.username)
-                && Objects.equals(password, appUser.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, email, username, password);
-    }
 }
+
---
+++ test/com/revature/banking/services/UserServiceTestSuite.java
@@ -0,0 +1,2 @@
+package com.revature.banking.services;public class UserServiceTestSuite {
+}
